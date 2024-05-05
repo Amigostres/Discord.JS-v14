68c63b5e037a97021659a6eb70df8f5f8ee72265
Jose <josemanuel21302@gmail.com>
2024-05-05 15:46:22 -0700

    basic discord bot replies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//the docs: https://discord.js.org/docs/packages/discord.js/14.15.0
 const { Client, IntentsBitField } = require('discord.js')
 //client is going to be our bot
 
@@ -16,6 +17,19 @@ const client = new Client({
 // when the bot turns on
 client.on('ready', e => { 
     console.log(`${e.user.tag} is on`) 
+})
+
+//basic message replies
+client.on('messageCreate', msg => {
+    if(msg.author.bot) return; //if the author is a bot then return. This onlt exist to not have the bot read it's own message
+
+    console.log('==================================');
+    //message docs
+    //https://discord.js.org/docs/packages/discord.js/14.15.0/Message:Class
+    console.log(`${msg.author.username} just said: ${msg.content}`);
+
+    msg.reply("I heard you!!")// reply to the user who sent the msg
+
 })
 
 client.login(process.env.TOKEN|| '') // <------ Enter TOKEN here!!
